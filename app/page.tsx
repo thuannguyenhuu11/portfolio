739789e0499a7962654d7a7b27003f999f7c2e65
Thuan Nguyen <thuannguyenhuu11@gmail.com>
2024-10-19 04:02:07 +0700

    feat: add navbar section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import HeroSection from '@/components/HeroSection/HeroSection';
+import NavBar from '@/components/Navbar/Navbar';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
+      <NavBar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
       </div>
