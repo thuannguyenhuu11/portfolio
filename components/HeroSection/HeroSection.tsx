1237ad8c7dc316df88bebc83b2c6f6f69a17fd35
Thuan Nguyen <thuannguyenhuu11@gmail.com>
2024-10-19 07:20:59 +0700

    feat: add resume into my portfolio

--- components/HeroSection/HeroSection.tsx
+++ components/HeroSection/HeroSection.tsx
@@ -37,7 +37,7 @@ const HeroSection = () => {
               Contact Me
             </Link>
             <Link
-              href="/"
+              href="https://drive.google.com/file/d/1AzUAam5S3lXvU6xcUZMNMc0vIMtDy7RI/view?usp=drive_link"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Download CV</span>
